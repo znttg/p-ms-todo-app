be75e040e3f553dd935507f073ed2f7d4ce32b38
Zanatta <zanattagiovani@gmail.com>
2025-01-28 02:04:55 -0300

    Add user info display on app page with authenticated session

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,12 @@
-export default function AppPage() {
-    return <div>App Page</div>
+import { auth } from "@/services/auth"
+import { UserInfo } from "./_components/user-info"
+
+export default async function AppPage() {
+    const session = await auth()
+    
+    return (
+        <main className="flex items-center justify-center h-screen">
+            <UserInfo user={session?.user} />
+        </main>
+    )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "./lib/get-url";
+
+export default function middleware(request: NextRequest) {
+    // get token from cookies
+    const token = request.cookies.get('authjs.session-token')
+    const pathname = request.nextUrl.pathname
+
+    console.log({
+        token: token?.value,
+        pathname
+    })
+
+    if (pathname === '/auth' && token) {
+        return NextResponse.redirect(new URL(getUrl('/app')))
+    }
+
+    if (pathname.includes('/app') && !token) {
+        return NextResponse.redirect(new URL(getUrl('/auth')))
+    }
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
